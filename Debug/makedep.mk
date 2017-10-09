b7a1a8d2a171c15a09ce10734f1a358959fc020e
MrBreadbox <32449164+MrBreadbox@users.noreply.github.com>
2017-10-09 16:55:28 +0200

    First Build

---
+++ Debug/makedep.mk
@@ -0,0 +1,22 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Data_Latch_V2.1.c
+
+Encoder.c
+
+FH5401C.c
+
+init.c
+
+Modus.c
+
+RS232.c
+
+shiftregister.c
+
+SPI.c
+
+Sram.c
+
